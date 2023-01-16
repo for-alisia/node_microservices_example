aba96cd9c8b01c249626f9bf983d3b0f6dd26c88
Elena <for-alisia@yandex.ru>
2023-01-16 16:03:51 +0100

    Added ingress config

--- client/src/CommentCreate.jsx
+++ client/src/CommentCreate.jsx
@@ -11,7 +11,7 @@ const CommentCreate = ({ postId }) => {
   const onSubmit = async (e) => {
     e.preventDefault();
 
-    await axios.post(`http://localhost:4001/posts/${postId}/comments`, { content: comment });
+    await axios.post(`http://posts.com/posts/${postId}/comments`, { content: comment });
 
     setComment('');
   }
